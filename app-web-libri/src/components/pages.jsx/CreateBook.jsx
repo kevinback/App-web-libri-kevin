7995aabdcb17eb5fbac891a536c37f1bc72afe86
Kevin <kevin.costa.japa@gmail.com>
2025-04-28 21:59:46 -0300

    Implementação do sistema de Navegação

---
+++ app-web-libri/src/components/pages.jsx/CreateBook.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import style from './Createbooks.module.css'
+const Createbooks = () => {
+    return (
+        <section className={style.create_book_container}>
+            <h1>CREATE BOOKS</h1>
+        </section>
+    )
+}
+export default Createbooks
---
+++ app-web-libri/src/components/pages.jsx/Home.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+import style from "./Home.module.css"
+ 
+const Home = ()=>{
+    return(
+        <section>
+ 
+            <h1>Bem vindo ao<span>LIBRI!</span></h1>
+            <p>Sua plataforma de gest~ao de livros!</p>
+            <img src="./book_home.jpg"/>
+ 
+        </section>
+    )
+}
+ 
+export default Home 
